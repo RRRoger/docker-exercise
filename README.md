144dd8efafad36d042d2e008d55c655469c5f1c1
chenpeng <chenpeng.0418@163.com>
2021-07-16 18:47:17 +0800

    [ADD] add scripts for `Task research baidu`

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# docker-exercise
-docker-exercise
+# docker练习 / docker-exercise
+[![](https://img.shields.io/badge/version-python3.x-green?style=flat-square)](https://www.python.org/downloads/)
+[![GitHub last commit](https://img.shields.io/github/stars/RRRoger/docker-exercise.svg?style=flat-square)](https://github.com/RRRoger/docker-exercise)
+[![GitHub issues](https://img.shields.io/github/issues/RRRoger/docker-exercise.svg?style=flat-square)](https://github.com/RRRoger/docker-exercise/issues)
+[![GitHub last commit](https://img.shields.io/github/last-commit/RRRoger/docker-exercise.svg?style=flat-square)](https://github.com/RRRoger/docker-exercise/commits/master)
+[![GitHub](https://img.shields.io/github/license/mashape/apistatus.svg?style=flat-square)](https://github.com/RRRoger/docker-exercise/blob/master/LICENSE)
+
+
+
+## 1. 制作docker镜像
+
+> 定时爬取百度页面,转成pdf保存在本地目录
+
---
+++ task_research_baidu/task_save_baidu_research.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+"""
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+    Task to research baidu with your keyword, 
+        And then save it as pdf on your local device.
+
+    Author: Roger
+    Python Version: python3+
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+"""
+
+import click
+import datetime
+import sys
+import os
+from os import path as osp
+from urllib.parse import urlencode, quote_plus
+
+
+# Page set
+PAGES = [0, 1, 2]
+
+WKHTMLTOPDF = "/usr/local/bin/wkhtmltopdf"
+URL = "https://www.baidu.com/s?wd={wd}&pn={pn}"
+
+help_c = """Task to research baidu with your keyword, and then save it as pdf."""
+help_output = "Output directory, like: ~/Desktop."
+help_wd = "The keyword what you want to resarch."
+
+@click.command()
+@click.help_option("-h", "--help", help=help_c)
+@click.option("-o", "--output-dir", "output_dir", help=help_output, type=str, default="./OUTPUT")
+@click.option("-wd", "--keyword", "keyword", help=help_wd, type=str, required=True)
+def main(output_dir, keyword):
+
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    wd = quote_plus(keyword)
+    today = datetime.datetime.now().strftime('%Y-%m-%d')
+    file_prefix = f"{keyword}-{today}"
+
+    for page in PAGES:
+        this_file_name = f"{file_prefix}_page{page+1}.pdf"
+        pn = page * 10
+        url = URL.format(wd=wd, pn=pn)
+        path = osp.join(output_dir, this_file_name)
+        cmd = f'{WKHTMLTOPDF} "{url}" "{path}"'
+        print(cmd)
+        os.system(cmd)
+
+    print("All are Done.....")
+
+if __name__ == '__main__':
+    main()
+
