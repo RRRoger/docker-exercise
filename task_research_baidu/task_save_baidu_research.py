c2139153b675e7ee943ef4237b4e2d1e759ca86b
chenpeng <chenpeng.0418@163.com>
2021-07-23 17:57:28 +0800

    [FEAT] 添加log

--- task_research_baidu/task_save_baidu_research.py
+++ task_research_baidu/task_save_baidu_research.py
@@ -16,6 +16,23 @@ import sys
 import os
 from os import path as osp
 from urllib.parse import quote_plus
+from subprocess import PIPE, Popen
+
+SUCCESS, ERROR, WARNING = 'SUCCESS', 'ERROR', 'WARNING'
+def _log(info, directory, level=SUCCESS):
+    print(info)
+    log_path = osp.join(directory, 'log.log')
+    with open(log_path, 'a+') as f:
+        f.write('%s %s %s\n' % (level, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), info))
+
+
+def cmdline(command):
+    process = Popen(
+        args=command,
+        stdout=PIPE,
+        shell=True
+    )
+    return process.communicate()[0]
 
 
 # Page set
@@ -36,11 +53,21 @@ def test_new_file(path):
     return True
 
 
+def getsize(path):
+    """ return float with UNIT KB """
+    if os.path.exists(path):
+        return round(os.path.getsize(path) / 1024, 2)
+    else:
+        return 0
+
+
 @click.command()
 @click.help_option("-h", "--help", help=help_c)
 @click.option("-o", "--output-dir", "output_dir", help=help_output, type=str, default="./OUTPUT")
 @click.option("-wd", "--keyword", "keyword", help=help_wd, type=str, required=True)
 def main(output_dir, keyword):
+
+    _log("Start to research .....", output_dir)
 
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
@@ -54,12 +81,16 @@ def main(output_dir, keyword):
         pn = page * 10
         url = URL.format(wd=wd, pn=pn)
         path = osp.join(output_dir, this_file_name)
-        cmd = f'{WKHTMLTOPDF} "{url}" "{path}"'
-        print(cmd)
-        # test_new_file(path)
-        os.system(cmd)
+        cmd = f'{WKHTMLTOPDF} "{url}" "{path}"'        # cmd = 'echo "Hello World!!"'
+        _log(cmd, output_dir)
 
-    print("All are Done.....")
+        # execute command
+        var = cmdline(cmd)
+        
+        fsize = getsize(path)
+        _log(f"[{this_file_name}]  {fsize}KB", output_dir)
+
+    _log("All are done.....", output_dir)
 
 if __name__ == '__main__':
     main()
